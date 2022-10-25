df0fc637b5c25a6338aba1b71c1aa527aae76ed4
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-10-25 13:59:52 -0700

    added bold passage
    Co-authored-by: Reed Carter <reedandrew18@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 //Business Logic
 
 function numberOfOccurrencesInText(word, text) {
+  if (word.trim().length === 0) {
+    return 0;
+  }
   const textArray = text.split(" ");
   let wordCount = 0;
   textArray.forEach(function(element) {
@@ -39,6 +42,27 @@ function removeWords(text) {
   outputText.trim();
   return outputText;
 }
+function boldPassage(word, text) {
+  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+    return null;
+  }
+  const p = document.createElement("p");
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (word === element) {
+      const bold = document.createElement("strong");
+      bold.append(element);
+      p.append(bold);
+    } else {
+      p.append(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      p.append(" ");
+    }
+  });
+  return p;
+}
+
 
 // UI Logic
 function handleFormSubmission(event) {
