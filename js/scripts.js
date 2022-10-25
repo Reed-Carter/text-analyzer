f13662c7f348b776bd6231711c4843c78a561db7
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-10-25 13:23:41 -0700

    added ui logic
    Co-authored-by: Reed Carter <reedandrew18@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ function numberOfOccurrencesInText(word, text) {
   const textArray = text.split(" ");
   let wordCount = 0;
   textArray.forEach(function(element) {
-    if (element.toLowerCase().includes(word.toLowerCase())) {
+    if (element.toLowerCase().includes(word.toLowerCase()) && word !== "") {
       wordCount++;
     }
   });
@@ -39,3 +39,18 @@ function removeWords(text) {
   outputText.trim();
   return outputText;
 }
+
+// UI Logic
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
